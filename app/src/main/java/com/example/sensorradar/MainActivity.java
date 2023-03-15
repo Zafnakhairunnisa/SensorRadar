4804802942c875f65d2bad502e41f8830d64438c
Zafnakhairunnisa <zafna.khairunnisa@mail.ugm.ac.id>
2023-03-15 14:38:06 +0700

    Initial commit

--- app/src/main/java/com/example/sensorradar/MainActivity.java
+++ app/src/main/java/com/example/sensorradar/MainActivity.java
@@ -2,13 +2,95 @@ package com.example.sensorradar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+
+    private Sensor mSensorAccelerometer;
+    private Sensor mSensorMagnetometer;
+
+    private TextView mTextSensorAzimuth, mTextSensoPitch, mTextSensrRoll;
+
+    private float[] mAccelerometerData = new float[3];
+    private float[] mMagnetometerData = new float[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        mTextSensorAzimuth = findViewById(R.id.value_azimuth);
+        mTextSensrRoll = findViewById(R.id.value_roll);
+        mTextSensoPitch = findViewById(R.id.value_pitch);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        mSensorAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+    }
+
+    protected void onStart() {
+        super.onStart();
+        if (mSensorAccelerometer != null) {
+            mSensorManager.registerListener(this, mSensorAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if (mSensorMagnetometer != null) {
+            mSensorManager.registerListener(this, mSensorMagnetometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        int sensorType = sensorEvent.sensor.getType();
+
+        switch (sensorType) {
+            case Sensor.TYPE_ACCELEROMETER:
+                mAccelerometerData = sensorEvent.values.clone();
+                break;
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                mMagnetometerData = sensorEvent.values.clone();
+                break;
+            default:
+                return;
+        }
+
+        float [] rotationMatrix = new float[9];
+        boolean rotationOk = SensorManager.getRotationMatrix(rotationMatrix, null, mAccelerometerData, mMagnetometerData);
+        float orientationValues [] = new float[3];
+        if (rotationOk) {
+            SensorManager.getOrientation(rotationMatrix, orientationValues);
+        }
+
+        float azimuth = orientationValues[0];
+        float pitch = orientationValues[1];
+        float roll = orientationValues[2];
+
+        mTextSensorAzimuth.setText(getResources().getString(R.string.value_format, azimuth));
+        mTextSensoPitch.setText(getResources().getString(R.string.value_format, pitch));
+        mTextSensrRoll.setText(getResources().getString(R.string.value_format, roll));
+
+    }
+
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
